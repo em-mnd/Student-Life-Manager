e366ee139ba3b5b6c0796a292e08ba0ce1316dcb
Riri <167028773+em-mnd@users.noreply.github.com>
2026-08-19 16:34:04 +0200

    Update notes.py
    added menu for notes and categories, func: add_note, categories_menu, notes_menu

--- Features/notes.py
+++ Features/notes.py
@@ -1,20 +1,76 @@
+from datetime import datetime, date
 # A simple note space that will include categories and maybe a search function when I'll get more comfortable.
+notes_list = []
+categories = []
 
 def notes_menu():
     while True:
-        input("Welcome to the Notes feature! Please choose an option: \n1. Add Category\n2. Remove Category\n3. Update Category\n4. View Categories\n5. Add Notes\n6. Remove Notes\n7. Update Notes\n8. Exit\n")
-        note_choice = input().index[1:9]
+        notes_choice = input("Welcome to the Notes menu ! Please choose an option: \n1. Add note\n2. Remove note\n3. Update note\n4. View all notes\n5. Categories\n6. Exit")
+        if note_choice not in [1, 2, 3, 4, 5]:
+            print("Invalid input, please try again.")
+            return
+        elif notes_choice == '1':
+            add_note()
+        elif notes_choice == '2':
+            remove_note()
+        elif notes_choice == '3':
+            update_note()
+        elif notes_choice == '4':
+            view_all_notes()
+        elif notes_choice == '5':
+            categories_menu()
+        elif notes_choice == '6':
+            print('Exiting the Notes menu. Returning to main menu...')
+            return
+
+def categories_menu():
+    while True:
+        categories_choice = input('Here is the Categories menu for notes. Please choose an option: \n1. Add category\n2. Remove category\n3. Update category\n4. View all categories\n5. Exit')
+        if categories_choice not in [1, 2, 3, 4 ,5]:
+            print("Invalid input, please try again.")
+            return
+        elif categories_choice == '1':
+            add_category()
+        elif categories_choice == '2':
+            remove_category()
+        elif categories_choice == '3':
+            update_category()
+        elif categories_choice =='4':
+            view_all_categories()
+        elif categories_choice == '5':
+            print('Exiting Categories menu. Returning to Notes menu...')
+            return
+
+def add_notes():
+    add_note_choice = input("Do you want to add a note ? (y/n): ").lower().strip()
+    if add_note_choice == 'n':
+        print("Returning to Notes menu...")
+        return
+    if add_note_choice != 'y':
+        print("Invalid input, returning to Notes menu...")
+        return
+    if add_note_choice == 'y':
         while True:
-            if input() in note_choice not in [1, 2, 3, 4, 5, 6, 7, 8]:
-                print("Invalid input, please try again.")
-            else:
-                pass
+            note_title = input("Enter the note title: ").strip()
+            if note_title == '':
+                print('Please enter a title for your note.')
+                continue
+            break
+        note_content = input(
+            "Write your note here: "
+        ).strip()
+        try:
+            len(note_content) > 100
+        except ValueError:
+            print("Your note exceeds the limited amount of characters.")
+            continue
+        else:
+            notes_list.append(note_content)
+            print('Your note has been added successfully !')
+
+
 
 def add_category():
     pass
 # Categories : I'd rather have the notes separated, if i'd ever have to use a table it would be more efficient.
 # A limit of categories maybe ? Alphabetical order ?
-
-def add_notes():
-    pass
-# Thinking about adding a limit of characters, but I don't think that would be efficient. Maybe separate notes by most recent to oldest, or levels of importance then again would it be user friendly ?
